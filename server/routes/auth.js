dad516f0793ed51acec15171199674a6c58ed033
Ranavdiya Mohmedanas <143054234+MohmedAnas@users.noreply.github.com>
2026-01-05 11:43:48 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,13 @@
-const express = require('express');
-const { login } = require('../controllers/authController');
+const express = require("express");
+const { login } = require("../controllers/authController");
 
 const router = express.Router();
 
-router.post('/login', login);
+// 🔥 Handle preflight for login
+router.options("/login", (req, res) => {
+  res.sendStatus(200);
+});
+
+router.post("/login", login);
 
 module.exports = router;
