adcf22de66a9da8ae50133b550986264a25fe7b6
Ranavdiya Mohmedanas <143054234+MohmedAnas@users.noreply.github.com>
2026-01-05 14:25:29 +0530

    Disable student routes in server configuration
    Comment out student routes in server.js

--- server/server.js
+++ server/server.js
@@ -50,8 +50,8 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 
 // if you have students routes:
-const studentRoutes = require("./routes/students");
-app.use("/api/students", studentRoutes);
+//const studentRoutes = require("./routes/students");
+//app.use("/api/students", studentRoutes);
 
 // 7. 404 handler AFTER all routes
 app.use((req, res) => {
