efa0838531f5b02bf6fdb5aab754c8869de1cbbc
Ranavdiya Mohmedanas <143054234+MohmedAnas@users.noreply.github.com>
2026-01-05 14:05:25 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,31 +1,8 @@
 import axios from "axios";
 
-const API_URL =
-  process.env.REACT_APP_API_URL ||
-  "https://student-entry-management-system.onrender.com/api";
-
-// Create ONE axios instance
 const api = axios.create({
   baseURL: API_URL,
-  withCredentials: true // 🔥 THIS FIXES CORS
+  withCredentials: true
 });
 
-export const addStudent = async (data) => {
-  const response = await api.post("/students", data);
-  return response.data;
-};
-
-export const getStudents = async () => {
-  const response = await api.get("/students");
-  return response.data;
-};
-
-export const updateStudent = async (index, data) => {
-  const response = await api.put(`/students/${index}`, data);
-  return response.data;
-};
-
-export const login = async (credentials) => {
-  const response = await api.post("/auth/login", credentials);
-  return response.data;
-};
+export const login = (credentials) => api.post("/auth/login", credentials);
