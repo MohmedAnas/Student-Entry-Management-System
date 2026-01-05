9d75bc242bba06767a2f45921768fdfe77daae65
Ranavdiya Mohmedanas <143054234+MohmedAnas@users.noreply.github.com>
2026-01-05 14:12:36 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,8 +1,23 @@
-import axios from "axios";
+import axios from 'axios';
 
-const api = axios.create({
-  baseURL: API_URL,
-  withCredentials: true
-});
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
-export const login = (credentials) => api.post("/auth/login", credentials);
+export const addStudent = async (data) => {
+  const response = await axios.post(`${API_URL}/students`, data);
+  return response.data;
+};
+
+export const getStudents = async () => {
+  const response = await axios.get(`${API_URL}/students`);
+  return response.data;
+};
+
+export const updateStudent = async (index, data) => {
+  const response = await axios.put(`${API_URL}/students/${index}`, data);
+  return response.data;
+};
+
+export const login = async (credentials) => {
+  const response = await axios.post(`${API_URL}/auth/login`, credentials);
+  return response.data;
+};
